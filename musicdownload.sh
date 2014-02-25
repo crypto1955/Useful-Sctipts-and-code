bf876f57f0cd846f26669b6b4de97fd44979e92b
S Praveenkumar <sbpraveen34@gmail.com>
2014-02-25 17:41:15 +0000

    this is now contains my favo songs download scripts too

---
+++ musicdownload.sh
@@ -0,0 +1,36 @@
+
+    wget http://filemirchi.info/Tamil/S/Subramaniapuram/Kangal_Irandal-VmusiQ.Com.mp3  
+    wget http://filemirchi.info/Hindi/A/Aashiqui.2_160kbps/Tum_Hi_Ho-StarMusiQ.Com.mp3
+    wget http://filemirchi.info/Hindi/A/Aashiqui.2_160kbps/Sunn_Raha_Hai-StarMusiQ.Com.mp3
+    wget http://filemirchi.info/Hindi/A/Aashiqui.2_160kbps/Chahun_Main_Ya_Naa-StarMusiQ.Com.mp3
+    wget http://filemirchi.info/Hindi/A/Aashiqui.2_160kbps/Sunn_Raha_hai_II-StarMusiQ.Com.mp3
+    wget http://filemirchi.info/Hindi/A/Aashiqui.2_160kbps/Milne_Hai_Mujhse_Aayi-StarMusiQ.Com.mp3
+    wget http://sound17.mp3pk.com/indian/dummaarodum/dummaardum03(www.songs.pk).mp3
+    wget http://sound17.mp3pk.com/indian/dummaarodum/dummaardum03\(www.songs.pk\).mp3
+    mv dummaardum03\(www.songs.pk\).mp3 jiyen\ kyun.mp3
+    wget http://filemirchi.info/tamil/A/Aadhavan/Hasile_Fisile_VmusiQ.Com.mp3
+    wget http://filemirchi.info/tamil/A/Aadhavan/Vaarayo_Vaarayo_VmusiQ.Com.mp3
+    wget http://filemirchi.info/tamil/A/Aadhavan/Yeno_Yeno_Panithuli_VmusiQ.Com.mp3
+    wget http://filemirchi.info/Tamil/B/Bheema_160kbps/Mudhal%20Mazhai%20-%20VmusiQ.Com.mp3
+    wget http://filemirchi.info/Tamil/B/Bheema_160kbps/Ragasiya%20Kanavugal%20-%20VmusiQ.Com.mp3
+    wget http://filemirchi.info/Tamil/C/Chellame/Kadhalikkum_Asai-VmusiQ.Com.mp3
+    wget http://filemirchi.info/Tamil/C/Chellame/Aariya_Udhadugal-VmusiQ.Com.mp3
+    wget http://filemirchi.info/Tamil/E/Engeyum_Kadhal_160kbps/Thee_Illai-VmusiQ.Com.mp3
+    wget http://filemirchi.info/Tamil/G/Ghajini/Rahatulla-VmusiQ.Com.mp3
+    wget http://filemirchi.info/Tamil/G/Ghajini/Oru_Maalai-VmusiQ.Com.mp3
+    wget http://filemirchi.info/Tamil/K/KO/Ennamo_Yeadho-VmusiQ.Com.mp3
+    wget http://filemirchi.info/Tamil/K/Kaakha_Kaakha/Thoothu_Varuma-VmusiQ.Com.mp3
+    wget http://filemirchi.info/Tamil/M/Maatraan_160kbps/Rettai%20Kathirae-VmusiQ.Com.mp3
+    wget http://filemirchi.info/Tamil/M/Maatraan_160kbps/Theeyae%20Theeyae-VmusiQ.Com.mp3
+    wget http://filemirchi.info/Tamil/M/Majunu_160kbps/Gulmohar_Malarae-VmusiQ.Com.mp3
+    wget http://filemirchi.info/Tamil/M/Majunu_160kbps/Gulmohar_Malarae_II-VmusiQ.Com.mp3
+    wget http://filemirchi.info/Tamil/M/Majunu_160kbps/Mudhar_Kanavae-VmusiQ.Com.mp3
+    wget http://filemirchi.info/Tamil/M/Majunu_160kbps/Pada_Pada_Pattampoochi-VmusiQ.Com.mp3
+    wget http://filemirchi.info/telugu/O/Orange_2010/Rooba%20Rooba%20-%20VmusiQ.Com.mp3
+    wget http://filemirchi.info/Tamil/T/Thuppakki_160kbps/Antarctica-VmusiQ.Com.mp3
+    wget http://filemirchi.info/Tamil/T/Thuppakki_160kbps/Alaikaa_Laikaa-VmusiQ.Com.mp3
+    wget http://filemirchi.info/Tamil/S/Sathyam/En_Aanbe-VmusiQ.Com.mp3
+    wget http://filemirchi.info/Tamil/U/Unnale_Unnale_160kbps/June_Pona-StarMusiQ.Com.mp3
+    wget http://filemirchi.info/Tamil/V/Vettaiyadu_Vilayadu/Karka_Karka-VmusiQ.Com.mp3
+    wget http://filemirchi.info/Tamil/V/Vettaiyadu_Vilayadu/Manjal_Veiyil-VmusiQ.Com.mp3
+ 
